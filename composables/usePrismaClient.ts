670045979a7438bcfc05361307775b622a4e9498
chuancheng.hua@razer.com <chuancheng.hua@razer.com>
2025-06-26 11:54:36 +0800

    fix: remove unuse

--- composables/usePrismaClient.ts
+++
@@ -1,6 +0,0 @@
-import { useNuxtApp } from '#imports'
-import type { CustomPrismaClient } from '~/server/libs/prisma'
-
-export function usePrismaClient() {
-  return useNuxtApp().$prisma as CustomPrismaClient
-}
